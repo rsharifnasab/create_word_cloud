593363a5e8d3f74cd7800a151754118c3a2a2179
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2020-02-29 15:04:12 +0330

    added close file option to telegram version, added some dicstrings

--- telegram/telegram_abr.py
+++ telegram/telegram_abr.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+##############################
+#          IMPORTS
+##############################
 from bs4 import BeautifulSoup
 from os import system as shell
 from glob import glob as glob_path
@@ -7,15 +10,17 @@ from wordcloud_fa import WordCloudFa
 from numpy import array as np_array
 from PIL import Image
 
+######################################
+#             CONFIGS
+######################################
+MASK = "../assets/masks/telegram.png"
+FONT = "../assets/fonts/IR/IRNazanin_YasDL.com.ttf"
+
 DEFAULT_INPUT_PATH = './chat-datas/*/*.html'
 RESULT_FILE_ADD = "./result.png"
 
-MASK = "../assets/masks/telegram.png"
-
-FONT = "../assets/fonts/IR/IRNazanin_YasDL.com.ttf"
-
-
 BG_COLOR = "white"
+
 
 STOP_WRODS_LIST =[
     "../assets/stop_words/stopwords_me.txt",
@@ -23,42 +28,68 @@ STOP_WRODS_LIST =[
     "../assets/stop_words/addtional_stops.txt",
 ]
 
-
+#########################################
+#              the CODE
+#########################################
 def get_input_folder():
+    """
+    communicate with user and get address of srouce
+    """
     print("please enter path of exported folder")
-    print("(nothing for default)")
+    print(f"(nothing for default: {DEFAULT_INPUT_PATH})")
     custom_path = input().strip()
     final_path = custom_path+"/*.html" if custom_path !="" else DEFAULT_INPUT_PATH
     return final_path
 
 
 def load_stop_words():
+    """
+    load stop words and return them as a set
+    it load from 3 files that wrote in STOP_WORDS_LIST
+    """
     words = []
-    for file in STOP_WRODS_LIST:
-        new_words = open(file,"r").read().split()
-        words+= new_words
+    for file_add in STOP_WRODS_LIST:
+        with open(file_add,"r") as file:
+            new_words = file.read().split()
+            words+= new_words
     return set(words)
 
 def clean_word(d):
+    """
+        remove some bad words from input
+        for example twitter links
+        or remove nim fasele
+        or ...
+    """
     d.replace("\u200c","")
     if len(d) <3: return ""
     if "@" in d : return ""
     if "joinchat" in d : return ""
-    #if "بیش" in d  : return ""
-    #if "می" in d : return ""
-    #if d == "ست" : return ""
 
     return d
 
 
 def print_stats(text):
+    """
+    show some statistics
+    to make sure that the program opened correct input file
+    """
     print( f" len e kol : {len(text)}")
-    print (f"""spaces count : { len( text.split(" ") ) }""" )
+    print (f"""spaces count : { text.count(" ") }""" )
 
 
 list_of_files = glob_path( get_input_folder() )
+
 print("loading files")
-loaded_xml = "\n".join( [ open(add,"r").read() for add in list_of_files ] )
+xml_list = []
+for file_add in list_of_files:
+    with open(file_add,"r") as file:
+        xml_list.append(file.read())
+loaded_xml = "\n".join(xml_list)
+
+if loaded_xml.strip() == "":
+    print("nothing loaded, exiting...")
+    exit()
 
 print("parsing")
 soup = BeautifulSoup(loaded_xml, 'html.parser')
